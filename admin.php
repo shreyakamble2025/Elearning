0871a7505ac867283d47584898c5ed398d051770
shreyakamble2025 <shreyakamble2025@gmail.com>
2026-07-30 10:30:22 +0530

    add deptwise files

---
+++ admin.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<td>
+    <a href="co.html">CO</a>
+    <a href="aiml.html">AIML</a>
+    <a href="me.html">ME</a>
+    <a href="ce.html">CE</a>
+    <a href="ee.html">EE</a>
+</td>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<a href="admin.php">admin</a>
+<a href="teacher.php">teacher</a>
+<a href="student.php">student</a>
+</body>
+</html>
